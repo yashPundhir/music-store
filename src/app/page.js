12b300a0294d1898b27fb8ed50cdc12f6aefb6c1
yashPundhir <yash.pundhir.prof@gmail.com>
2025-01-06 22:06:38 +0530

    add hero section to page.js file

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
-import Image from "next/image";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
 	return (
 		<main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-			<h1>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, neque.
-			</h1>
+			<HeroSection />
 		</main>
 	);
 }
