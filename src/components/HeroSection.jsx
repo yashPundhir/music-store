1e755a9598e046df6c93540850e38f25e6263b7a
yashPundhir <yash.pundhir.prof@gmail.com>
2025-01-08 23:52:40 +0530

    update hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const HeroSection = () => {
 	return (
 		<div className="text-white bg-red-200 px-10 py-2 rounded-md">
-			HeroSection
+			HeroSection updated
 		</div>
 	);
 };
